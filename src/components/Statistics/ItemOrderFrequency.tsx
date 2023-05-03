d4d40d431615d3a5cf3adcfba1a0114897628488
Lisa <lis-zaa@live.com>
2023-05-03 14:15:50 -0500

    added select option on statistics to scroll into any graph on select

--- src/components/Statistics/ItemOrderFrequency.tsx
+++ src/components/Statistics/ItemOrderFrequency.tsx
@@ -31,7 +31,7 @@ export default function ItemOrderFrequency() {
   return (
     <ResponsiveContainer
       width='100%'
-      height='100%'>
+      height='90%'>
       <BarChart
         layout='vertical'
         data={data}
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,16 +1,48 @@
 import './statistics.scss';
 import ItemOrderFrequency from './ItemOrderFrequency';
+import { useRef } from 'react';
 
 export default function Statistics() {
+  const IOFref = useRef<null | HTMLDivElement>(null);
+  const testref = useRef<null | HTMLDivElement>(null);
+
+  const graphs = [
+    { ref: IOFref, name: 'Item Order Frequency' },
+    { ref: testref, name: 'test' },
+  ];
+
+  const onSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const r = graphs[+e.target.value].ref;
+    r.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div id='statistics-container'>
       <div className='header'>Statistics</div>
+      <select
+        name='graph-select'
+        id='graph-select'
+        onChange={(e) => onSelect(e)}>
+        {graphs.map((graph, index) => {
+          return (
+            <option
+              value={index}
+              key={index}>
+              {graph.name}
+            </option>
+          );
+        })}
+      </select>
       <div className='content-container'>
         <div className='graph-container'>
-          <h1>Individual Item Popularity</h1>
+          <h1 ref={IOFref}>Individual Item Popularity</h1>
           <ItemOrderFrequency />
         </div>
         <hr />
+        <div className='graph-container'>
+          <h1 ref={testref}>Test</h1>
+          <ItemOrderFrequency />
+        </div>
       </div>
     </div>
   );
